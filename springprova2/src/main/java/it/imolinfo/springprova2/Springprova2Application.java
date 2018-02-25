341126c804350762bf955d047cf8db622333cd24
Giuliano Santandrea <giuliano.santandrea2@gmail.com>
2018-02-25 22:10:45 +0100

    init

---
+++ springprova2/src/main/java/it/imolinfo/springprova2/Springprova2Application.java
@@ -0,0 +1,12 @@
+package it.imolinfo.springprova2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springprova2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springprova2Application.class, args);
+	}
+}
---
+++ springprova2/src/test/java/it/imolinfo/springprova2/Springprova2ApplicationTests.java
@@ -0,0 +1,16 @@
+package it.imolinfo.springprova2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springprova2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
